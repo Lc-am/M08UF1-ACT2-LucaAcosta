746f39cb020c1b41d5fdd2af90ff1fc51b7c4d75
Luca <lucamateo1110@gmail.com>
2024-11-07 10:36:23 +0100

    ejercicio 1
    scripts realizados, falta cámara

---
+++ M08UF1-ACT2-LucaAcosta/Assets/Scripts/HitCollider.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+public class HitCollider : MonoBehaviour
+{
+    public UnityEvent<HitCollider, HurtCollider> onHitDelivered;
+    public List<string> hittableTags;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hittableTags.Contains(other.gameObject.tag))
+        {
+            HurtCollider hurtCollider = other.GetComponent<HurtCollider>();
+
+            if (hurtCollider != null)
+            {
+                hurtCollider.NotifyHit(this);
+                onHitDelivered?.Invoke(this, hurtCollider);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (hittableTags.Contains(collision.gameObject.tag))
+        {
+            HurtCollider hurtCollider = collision.gameObject.GetComponent<HurtCollider>();
+
+            if (hurtCollider != null)
+            {
+                hurtCollider.NotifyHit(this);
+                onHitDelivered?.Invoke(this, hurtCollider);
+            }
+        }
+    }
+}
