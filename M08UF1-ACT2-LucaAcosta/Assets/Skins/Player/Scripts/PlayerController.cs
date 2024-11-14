a2c8825e3c167b483ac5ad4293c9029b2d0f8105
Luca Mateo Acosta Servian <lucamateo1110@gmail.com>
2024-11-14 22:56:24 +0100

    script playercontroller

--- M08UF1-ACT2-LucaAcosta/Assets/Skins/Player/Scripts/PlayerController.cs
+++ M08UF1-ACT2-LucaAcosta/Assets/Skins/Player/Scripts/PlayerController.cs
@@ -1,42 +1,92 @@
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
+    [Header("Movimiento de Avance")]
+    [SerializeField] public float velocidadPlayer; // Vfwd
+    [SerializeField] public float incrementoVelocidad; //dV
+    [SerializeField] public float velocidadMaxima; 
+    [SerializeField] public float velocidadMinima;
 
-    private void Start()
+    [Header("Movimiento vertical")]
+    [SerializeField] public float velocidadVertical; //Vvert
+    [SerializeField] public float gravedad = -9.81f;
+    
+    public bool isGrounded;
+
+    private Rigidbody rb;
+
+    void Start()
     {
-        controller = gameObject.AddComponent<CharacterController>();
+        isGrounded = false;
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
-
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
-
-        // Makes the player jump
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
-        }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        HandleForwardMovement();
+        HandleVerticalMovement();
     }
+
+    private void HandleForwardMovement()
+    {
+        // Si la velocidad es menor que Vmin, incrementa hasta Vmin
+        if (velocidadPlayer < velocidadMinima)
+        {
+            velocidadPlayer = velocidadMinima;
+        }
+
+        // Aplica la velocidad hacia adelante
+        Vector3 forwardMovement = transform.forward * velocidadPlayer * Time.deltaTime;
+        rb.MovePosition(rb.position + forwardMovement);
+    }
+
+    public void IncreaseForwardSpeed()
+    {
+        // Incremento fijo de velocidad hasta un máximo
+        velocidadPlayer = Mathf.Min(velocidadPlayer + incrementoVelocidad, velocidadMaxima);
+    }
+
+    public void HalveForwardSpeed()
+    {
+        // Reduce la velocidad a la mitad
+        velocidadPlayer /= 2f;
+    }
+
+    private void HandleVerticalMovement()
+    {
+        // Aplicar gravedad mientras no esté en el suelo
+        if (!isGrounded)
+        {
+            velocidadVertical += gravedad * Time.deltaTime;
+        }
+
+        // Aplica la velocidad vertical
+        Vector3 verticalMovement = new Vector3(0, velocidadVertical * Time.deltaTime, 0);
+        rb.MovePosition(rb.position + verticalMovement);
+    }
+
+    public void SetVerticalSpeed(float newVerticalSpeed)
+    {
+        // Modifica la velocidad vertical instantáneamente
+        velocidadVertical = newVerticalSpeed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Comprueba si toca el suelo
+        if (collision.contacts[0].point.y <= transform.position.y)
+        {
+            isGrounded = true;
+            velocidadVertical = 0f; // Reinicia la velocidad vertical al tocar el suelo
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        // Sale del suelo
+        isGrounded = false;
+    }
+
 }
