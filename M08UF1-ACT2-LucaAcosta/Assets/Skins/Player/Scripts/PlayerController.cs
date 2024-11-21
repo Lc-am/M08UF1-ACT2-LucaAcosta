ccdbf3f7db8d262150711fa0093967a9f3e05724
luca <lucamateo1110@gmail.com>
2024-11-21 09:58:43 +0100

    Loading Scene

--- M08UF1-ACT2-LucaAcosta/Assets/Skins/Player/Scripts/PlayerController.cs
+++ M08UF1-ACT2-LucaAcosta/Assets/Skins/Player/Scripts/PlayerController.cs
@@ -31,61 +31,52 @@ public class PlayerController : MonoBehaviour
 
     private void HandleForwardMovement()
     {
-        // Si la velocidad es menor que Vmin, incrementa hasta Vmin
         if (velocidadPlayer < velocidadMinima)
         {
             velocidadPlayer = velocidadMinima;
         }
 
-        // Aplica la velocidad hacia adelante
         Vector3 forwardMovement = transform.forward * velocidadPlayer * Time.deltaTime;
         rb.MovePosition(rb.position + forwardMovement);
     }
 
     public void IncreaseForwardSpeed()
     {
-        // Incremento fijo de velocidad hasta un máximo
         velocidadPlayer = Mathf.Min(velocidadPlayer + incrementoVelocidad, velocidadMaxima);
     }
 
     public void HalveForwardSpeed()
     {
-        // Reduce la velocidad a la mitad
         velocidadPlayer /= 2f;
     }
 
     private void HandleVerticalMovement()
     {
-        // Aplicar gravedad mientras no esté en el suelo
         if (!isGrounded)
         {
             velocidadVertical += gravedad * Time.deltaTime;
         }
 
-        // Aplica la velocidad vertical
         Vector3 verticalMovement = new Vector3(0, velocidadVertical * Time.deltaTime, 0);
         rb.MovePosition(rb.position + verticalMovement);
     }
 
     public void SetVerticalSpeed(float newVerticalSpeed)
     {
-        // Modifica la velocidad vertical instantáneamente
         velocidadVertical = newVerticalSpeed;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        // Comprueba si toca el suelo
         if (collision.contacts[0].point.y <= transform.position.y)
         {
             isGrounded = true;
-            velocidadVertical = 0f; // Reinicia la velocidad vertical al tocar el suelo
+            velocidadVertical = 0f; 
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        // Sale del suelo
         isGrounded = false;
     }
 
