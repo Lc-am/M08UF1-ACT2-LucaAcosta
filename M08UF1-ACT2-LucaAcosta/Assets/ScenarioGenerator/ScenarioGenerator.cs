bbc4e999f73477a8f4f95415ddbf9af083036741
Lc-am <lucamateo1110@gmail.com>
2024-12-12 11:33:17 +0100

    autogenerado

---
+++ M08UF1-ACT2-LucaAcosta/Assets/ScenarioGenerator/ScenarioGenerator.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class ScenarioGenerator : MonoBehaviour
+{
+    static public ScenarioGenerator instance;
+    [SerializeField] int numPiecesTogenerateOnStart = 4;
+    [SerializeField] GameObject[] piecesPrefabs;
+
+    [Header("Debug")]
+    [SerializeField] bool debugEndOfPieceReached;
+
+    Transform nextPiecePosition;
+
+    int numPiecesFinished = 0;
+
+    private void OnValidate()
+    {
+        if(debugEndOfPieceReached)
+        {
+            debugEndOfPieceReached = false;
+            EndOfPieceReached();
+        }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+        nextPiecePosition = transform;
+    }
+    private void Start()
+    {
+        for (int i = 0; i < numPiecesTogenerateOnStart; i++)
+        {
+            AddNewPiece();
+        }
+    }
+
+    public void EndOfPieceReached()
+    {
+        numPiecesFinished++;
+        if (numPiecesFinished > 1)
+        {
+            DestroyOldestPiece();
+            AddNewPiece();
+        }
+    }
+
+    void AddNewPiece()
+    {
+        GameObject piecesToInstantiate = piecesPrefabs[Random.Range(0, piecesPrefabs.Length)];
+        GameObject newPiece = Instantiate(piecesToInstantiate, nextPiecePosition.position, nextPiecePosition.rotation, transform);
+        nextPiecePosition = newPiece.GetComponentInChildren<NextPiece>().transform;
+    }
+
+    void DestroyOldestPiece()
+    {
+        GameObject oldestPiece = transform.GetChild(0).gameObject;
+        Destroy(oldestPiece);
+    }
+}
